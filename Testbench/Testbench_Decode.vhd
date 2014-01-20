4efe3efd89817ce05a9b442d3a7a43d472f934af
Jun Mendoza <jun.mendoza@autodesk.com>
2014-01-21 01:17:33 +0800

    Modify Decode test to integrate addr_mode and shifter

--- Testbench/Testbench_Decode.vhd
+++ Testbench/Testbench_Decode.vhd
@@ -42,15 +42,18 @@ ARCHITECTURE behavior OF Testbench_Decode IS
  
     COMPONENT Decode
     PORT(
-         instruction : IN  std_logic_vector(31 downto 0);
-         mem_regs : IN  t_MemRegister_15_32;
-         op_alu : OUT  std_logic_vector(7 downto 0);
-         op_branch : OUT  std_logic_vector(7 downto 0);
-         op_mem : OUT  std_logic_vector(7 downto 0);
-         op_system : OUT  std_logic_vector(7 downto 0);
-         operand1 : OUT  std_logic_vector(31 downto 0);
-         operand2 : OUT  std_logic_vector(31 downto 0);
-         operand3 : OUT  std_logic_vector(31 downto 0)
+			instruction : in STD_LOGIC_VECTOR(31 downto 0); 
+			mem_regs : in t_MemRegister_15_32;
+			op_alu : out STD_LOGIC_VECTOR(7 downto 0);  
+			op_branch : out STD_LOGIC_VECTOR(7 downto 0); 
+			op_mem : out STD_LOGIC_VECTOR(7 downto 0); 
+			op_system : out STD_LOGIC_VECTOR(7 downto 0);
+			Rd_addr : out STD_LOGIC_VECTOR(3 downto 0);
+			Rd : out STD_LOGIC_VECTOR(31 downto 0);
+			Rn : out STD_LOGIC_VECTOR(31 downto 0);
+			op3 : out STD_LOGIC_VECTOR(31 downto 0);
+			shifter : out STD_LOGIC_VECTOR(11 downto 0);
+			addr_mode : out STD_LOGIC_VECTOR(11 downto 0)
         );
     END COMPONENT;
     
@@ -82,9 +85,12 @@ ARCHITECTURE behavior OF Testbench_Decode IS
    signal op_branch : std_logic_vector(7 downto 0);
    signal op_mem : std_logic_vector(7 downto 0);
    signal op_system : std_logic_vector(7 downto 0);
-   signal operand1 : std_logic_vector(31 downto 0);
-   signal operand2 : std_logic_vector(31 downto 0);
-   signal operand3 : std_logic_vector(31 downto 0);
+   signal Rd_addr : std_logic_vector(31 downto 0);
+   signal Rd : std_logic_vector(31 downto 0);
+   signal Rn : std_logic_vector(31 downto 0);
+   signal op3 : std_logic_vector(31 downto 0);
+   signal shifter : std_logic_vector(11 downto 0);
+   signal addr_mode : std_logic_vector(11 downto 0);
  
 BEGIN
  
@@ -96,22 +102,17 @@ BEGIN
           op_branch => op_branch,
           op_mem => op_mem,
           op_system => op_system,
-          operand1 => operand1,
-          operand2 => operand2,
-          operand3 => operand3
+          Rd_addr => Rd_addr,
+          Rn => Rn,
+          op3 =>op3,
+          shifter => shifter,
+          addr_mode => addr_mode
         );
 
    -- Stimulus process
    stim_proc: process
    begin		
 	
-		-- ldr
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rd			memory address
-		
-		-- str
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rs			memory address
 	
 		-- [0] ldr R5, %10	
 		-- [1] str R5, 0x0000000B
@@ -123,48 +124,48 @@ BEGIN
 		
 		-- [0] ldr R5, %10		
 		-- ldr
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rd			memory address
+		-- 31-24		23-20		19-16		15-12			11-0
+		-- opcode	cond		Rd			Reserved 	address mode
 		instruction <= "00001110000001010000000000001010"; 
 		wait for 10 ns;
 		
 
 		-- [1] str R5, 0x0000000B
 		-- str
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rs			memory address
-		instruction <= "00001111000001010001000000001011"; 
+		-- 31-24		23-20		19-16		15-12			11-0
+		-- opcode	cond		Rd			Reserved 	address mode
+		instruction <= "00001111000001010000000100001011"; 
 		wait for 10 ns;
 		
 		
 		-- [2] ldr R5, 0x0000000B
 		-- ldr
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rd			memory address
-		instruction <= "00001110000001010001000000001011"; 
+		-- 31-24		23-20		19-16		15-12			11-0
+		-- opcode	cond		Rd			Reserved 	address mode
+		instruction <= "00001110000001010000000100001011"; 
 		wait for 10 ns;
 		
 		
 		-- [3] ldr R6, %2		
 		-- ldr
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rd			memory address
+		-- 31-24		23-20		19-16		15-12			11-0
+		-- opcode	cond		Rd			Reserved 	address mode
 		instruction <= "00001110000001100000000000000010"; 
 		
 		
 		-- [4] add R5, R5, R6
 		-- add
-		-- 31-24		23-20		19-16		15-12		11-8 		7-0
-		-- opcode	cond		Rd			Rn1		Rn2		Reserved
-		instruction <= "00000000000001010101011000000000"; 
+		-- 31-24		23-20		19-16		15-12		11-0 		
+		-- opcode	cond		Rd			Rn			Shifter
+		instruction <= "00000000000001010101001000000110"; 
 		wait for 10 ns;
 		
 		
 		-- [5] str R5, 0x0000000F
 		-- str
-		-- 31-24		23-20		19-16		15-0
-		-- opcode	cond		Rs			memory address
-		instruction <= "00001111000001010001000000001111"; 
+		-- 31-24		23-20		19-16		15-12			11-0
+		-- opcode	cond		Rd			Reserved 	address mode
+		instruction <= "00001111000001010000000100001111"; 
 		wait for 10 ns;
 		
       wait;

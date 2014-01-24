31e48d857f3088999ad80723ca8141343cb15367
Jun Mendoza <jun.mendoza@autodesk.com>
2014-01-24 19:43:51 +0800

    Testbench for decoding opcodes

--- Testbench/Testbench_DecodeOpcode.vhd
+++ Testbench/Testbench_DecodeOpcode.vhd
@@ -2,15 +2,15 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   13:43:53 01/18/2014
+-- Create Date:   19:36:08 01/24/2014
 -- Design Name:   
--- Module Name:   C:/jun/sourcecode/Research/ProtoCPU/Testbench/Testbench_Decode.vhd
+-- Module Name:   D:/jun/sourcecode/Research/ProtoCPU/Testbench/Testbench_DecodeOpcode.vhd
 -- Project Name:  ProtoCPU
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
 -- 
--- VHDL Test Bench Created by ISE for module: Decode
+-- VHDL Test Bench Created by ISE for module: DecodeOpcode
 -- 
 -- Dependencies:
 -- 
@@ -31,90 +31,55 @@ USE ieee.std_logic_1164.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
-use work.cpu_types.all;
  
-ENTITY Testbench_Decode IS
-END Testbench_Decode;
+ENTITY Testbench_DecodeOpcode IS
+END Testbench_DecodeOpcode;
  
-ARCHITECTURE behavior OF Testbench_Decode IS 
+ARCHITECTURE behavior OF Testbench_DecodeOpcode IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT Decode
+    COMPONENT DecodeOpcode
     PORT(
-			instruction : in STD_LOGIC_VECTOR(31 downto 0); 
-			mem_regs : in t_MemRegister_15_32;
-			op_alu : out STD_LOGIC_VECTOR(7 downto 0);  
-			op_branch : out STD_LOGIC_VECTOR(7 downto 0); 
-			op_mem : out STD_LOGIC_VECTOR(7 downto 0); 
-			op_system : out STD_LOGIC_VECTOR(7 downto 0);
-			Rd_addr : out STD_LOGIC_VECTOR(3 downto 0);
-			Rd : out STD_LOGIC_VECTOR(31 downto 0);
-			Rn : out STD_LOGIC_VECTOR(31 downto 0);
-			op3 : out STD_LOGIC_VECTOR(31 downto 0);
-			shifter : out STD_LOGIC_VECTOR(11 downto 0);
-			addr_mode : out STD_LOGIC_VECTOR(11 downto 0)
+         instruction : IN  std_logic_vector(31 downto 0);
+         op_alu : OUT  std_logic_vector(7 downto 0);
+         op_branch : OUT  std_logic_vector(7 downto 0);
+         op_datamove : OUT  std_logic_vector(7 downto 0);
+         op_system : OUT  std_logic_vector(7 downto 0);
+         optype : OUT  std_logic_vector(3 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
    signal instruction : std_logic_vector(31 downto 0) := (others => '0');
-	
-	signal memregion_register : t_MemRegister_15_32 := 
-	(
-		X"00000000", 
-		X"00000000", 
-		X"00000000", 
-		X"00000000",
-		X"00000000", 
-		X"00000000", 
-		X"00000000", 
-		X"00000000",
-		X"00000000", 
-		X"00000000", 
-		X"00000000", 
-		X"00000000",
-		X"00000000", 
-		X"00000000", 
-		X"00000000"
-	);
 
  	--Outputs
    signal op_alu : std_logic_vector(7 downto 0);
    signal op_branch : std_logic_vector(7 downto 0);
-   signal op_mem : std_logic_vector(7 downto 0);
+   signal op_datamove : std_logic_vector(7 downto 0);
    signal op_system : std_logic_vector(7 downto 0);
-   signal Rd_addr : std_logic_vector(3 downto 0);
-   signal Rd : std_logic_vector(31 downto 0);
-   signal Rn : std_logic_vector(31 downto 0);
-   signal op3 : std_logic_vector(31 downto 0);
-   signal shifter : std_logic_vector(11 downto 0);
-   signal addr_mode : std_logic_vector(11 downto 0);
+   signal optype : std_logic_vector(3 downto 0);
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: Decode PORT MAP (
+   uut: DecodeOpcode PORT MAP (
           instruction => instruction,
-          mem_regs => memregion_register,
           op_alu => op_alu,
           op_branch => op_branch,
-          op_mem => op_mem,
+          op_datamove => op_datamove,
           op_system => op_system,
-          Rd_addr => Rd_addr,
-          Rn => Rn,
-          op3 =>op3,
-          shifter => shifter,
-          addr_mode => addr_mode
+          optype => optype
         );
+
+  
 
    -- Stimulus process
    stim_proc: process
    begin		
-	
-	
-		-- [0] ldr R5, %10	
+     
+	  -- [0] ldr R5, %10	
 		-- [1] str R5, 0x0000000B
 		-- [2] ldr R5, 0x0000000B
 		-- [3] ldr R6, %2	
@@ -134,40 +99,40 @@ BEGIN
 		-- str
 		-- 31-24		23-20		19-16		15-12			11-0
 		-- opcode	cond		Rd			Reserved 	address mode
-		--instruction <= "00001111000001010000000100001011"; 
-		--wait for 10 ns;
+		instruction <= "00001111000001010000000100001011"; 
+		wait for 10 ns;
 		
 		
 		-- [2] ldr R5, 0x0000000B
 		-- ldr
 		-- 31-24		23-20		19-16		15-12			11-0
 		-- opcode	cond		Rd			Reserved 	address mode
-		--instruction <= "00001110000001010000000100001011"; 
-		--wait for 10 ns;
+		instruction <= "00001110000001010000000100001011"; 
+		wait for 10 ns;
 		
 		
 		-- [3] ldr R6, %2		
 		-- ldr
 		-- 31-24		23-20		19-16		15-12			11-0
 		-- opcode	cond		Rd			Reserved 	address mode
-		--instruction <= "00001110000001100000000000000010"; 
-		--wait for 10 ns;
+		instruction <= "00001110000001100000000000000010"; 
+		wait for 10 ns;
 		
 		-- [4] add R5, R5, R6
 		-- add
 		-- 31-24		23-20		19-16		15-12		11-0 		
 		-- opcode	cond		Rd			Rn			Shifter
-		--instruction <= "00000000000001010101001000000110"; 
-		--wait for 10 ns;
+		instruction <= "00000000000001010101001000000110"; 
+		wait for 10 ns;
 		
 		
 		-- [5] str R5, 0x0000000F
 		-- str
 		-- 31-24		23-20		19-16		15-12			11-0
 		-- opcode	cond		Rd			Reserved 	address mode
-		--instruction <= "00001111000001010000000100001111"; 
-		--wait for 10 ns;
-		
+		instruction <= "00001111000001010000000100001111"; 
+		wait for 10 ns;
+
       wait;
    end process;
 
--- cpu_types.vhd
+++ cpu_types.vhd
@@ -12,22 +12,6 @@ use IEEE.STD_LOGIC_1164.all;
 
 package cpu_types is
 
--- type <new_type> is
---  record
---    <type_name>        : std_logic_vector( 7 downto 0);
---    <type_name>        : std_logic;
--- end record;
---
--- Declare constants
---
--- constant <constant_name>		: time := <time_unit> ns;
--- constant <constant_name>		: integer := <value;
---
--- Declare functions and procedure
---
--- function <function_name>  (signal <signal_name> : in <type_declaration>) return <type_declaration>;
--- procedure <procedure_name> (<type_declaration> <constant_name>	: in <type_declaration>);
---
 	
 	-- Declare the constant max size of the program data
 	type t_MemProgramData_32_32 is array (0 to 31) of STD_LOGIC_VECTOR(31 downto 0);
@@ -53,14 +37,6 @@ package cpu_types is
 	constant R15_addr : integer := 14;  -- GPR
 	
 	
---	-- command types
---	subtype cmd_type_size is STD_LOGIC_VECTOR(15 downto 0);
---	constant cmd_type_alu : cmd_type_size := X"0000";
---	constant cmd_type_br  : cmd_type_size := X"0001";
---	constant cmd_type_mem : cmd_type_size := X"0002";
---	constant cmd_type_sys : cmd_type_size := X"0003";
---	
---	
 	-- command id
 	subtype cmd_id_size is STD_LOGIC_VECTOR(7 downto 0);
 	constant alu_add  : cmd_id_size := X"00";
@@ -101,30 +77,5 @@ end cpu_types;
 
 package body cpu_types is
 
----- Example 1
---  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
---    variable <variable_name>     : <type_declaration>;
---  begin
---    <variable_name> := <signal_name> xor <signal_name>;
---    return <variable_name>; 
---  end <function_name>;
-
----- Example 2
---  function <function_name>  (signal <signal_name> : in <type_declaration>;
---                         signal <signal_name>   : in <type_declaration>  ) return <type_declaration> is
---  begin
---    if (<signal_name> = '1') then
---      return <signal_name>;
---    else
---      return 'Z';
---    end if;
---  end <function_name>;
-
----- Procedure Example
---  procedure <procedure_name>  (<type_declaration> <constant_name>  : in <type_declaration>) is
---    
---  begin
---    
---  end <procedure_name>;
  
 end cpu_types;

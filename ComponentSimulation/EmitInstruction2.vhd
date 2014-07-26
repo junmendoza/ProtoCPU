b08ea64716789e052e8147ec8df60e5bd381963b
Jun Mendoza <jun.mendoza@autodesk.com>
2014-07-26 14:07:31 +0800

    Fix emit instruction state

--- ComponentSimulation/EmitInstruction2.vhd
+++ ComponentSimulation/EmitInstruction2.vhd
@@ -56,11 +56,19 @@ constant CONFIG_ENTRYMODE_CLKWAIT 		: integer := 3000;
 constant CONFIG_DISPLAY_ONOFF_CLKWAIT 	: integer := 3000;
 constant CONFIG_CLEAR_DISPLAY_CLKWAIT 	: integer := 120000;
 
+
+
 constant WRITE_CLKWAIT : integer := 3000;
+
 
 type LCD_STATE is ( LCD_DISABLED, 
 						  LCD_ENABLED
 						);
+						
+-- Flag that the LCD has data to emit
+type LCD_WRITE_FLAG is ( LCD_WRITE_FLAG_ON, 
+								 LCD_WRITE_FLAG_OFF
+							  );
 
 type LCD_POWERON is (
 							LCD_POWERON_1,
@@ -89,13 +97,25 @@ type LCD_WRITE_STATE is (
 						 );
 
 
-signal lcdEnableState : LCD_STATE;
-signal lcdWriteState : LCD_WRITE_STATE;
-signal initLCDPowerOn : LCD_POWERON;
+signal lcdWriteflag 		: LCD_WRITE_FLAG;
+signal lcdEnableState 	: LCD_STATE;
+signal lcdWriteState	 	: LCD_WRITE_STATE;
+signal initLCDPowerOn 	: LCD_POWERON;
 
 begin
 
-	-- trigger this component only ony resetting and emitting an instruction
+	-- Process to handle incoming data to emit
+	process(reset, instruction)
+	begin
+		ResetState : if reset = '1' then
+			lcdWriteflag <= LCD_WRITE_FLAG_OFF;
+		elsif reset = '0' then
+			lcdWriteflag <= LCD_WRITE_FLAG_ON;
+		end if ResetState;
+	end process;
+	
+	
+	-- Process to handle LCD state
 	process(clock, reset)
 	
 	variable clockCycles : integer;
@@ -103,169 +123,170 @@ begin
 	begin
 		ClockSync : if rising_edge(clock) then
 			ResetState : if reset = '1' then
+				clockCycles := 0;
 				lcdEnableState <= LCD_DISABLED;
 			elsif reset = '0' then
-				LCDEnableDisable : if lcdEnableState = LCD_DISABLED then
-					
-					PowerOnState : if initLCDPowerOn = LCD_POWERON_1 then
-						if clockCycles > POWERON_CLKWAIT_1 then
-							clockCycles := 0;
-							initLCDPowerOn <= LCD_POWERON_2;
-							end if;
-						elsif initLCDPowerOn = LCD_POWERON_2 then
-							if clockCycles < POWERON_CLKWAIT_2 then
-								LCDDataBus(7 downto 4) <= "0011";	
-								LCD_E <= '1';			-- Set pulse high
-							else
-								LCDDataBus(7 downto 4) <= "0000";	
-								LCD_E <= '0';			-- Set pulse low
+				IfThereIsDataToEmit : if lcdWriteflag = LCD_WRITE_FLAG_ON then
+					IsReadyForWrite : if lcdEnableState = LCD_DISABLED then
+						IsPoweredOn : if initLCDPowerOn = LCD_POWERON_1 then
+							if clockCycles > POWERON_CLKWAIT_1 then
 								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_3;
-							end if;
+								initLCDPowerOn <= LCD_POWERON_2;
+								end if;
+							elsif initLCDPowerOn = LCD_POWERON_2 then
+								if clockCycles < POWERON_CLKWAIT_2 then
+									LCDDataBus(7 downto 4) <= "0011";	
+									LCD_E <= '1';			-- Set pulse high
+								else
+									LCDDataBus(7 downto 4) <= "0000";	
+									LCD_E <= '0';			-- Set pulse low
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_3;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_3 then
+								if clockCycles > POWERON_CLKWAIT_3 then
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_4;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_4 then
+								if clockCycles < POWERON_CLKWAIT_4 then
+									LCDDataBus(7 downto 4) <= "0011";
+									LCD_E <= '1';			-- Set pulse high
+								else
+									LCDDataBus(7 downto 4) <= "0000";	
+									LCD_E <= '0';			-- Set pulse low
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_5;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_5 then
+								if clockCycles > POWERON_CLKWAIT_5 then
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_6;
+								end if;
+							elsif initLCDPowerOn = LCD_POWERON_6 then
+								if clockCycles < POWERON_CLKWAIT_6 then
+									LCDDataBus(7 downto 4) <= "0011";
+									LCD_E <= '1';			-- Set pulse high
+								else
+									LCDDataBus(7 downto 4) <= "0000";	
+									LCD_E <= '0';			-- Set pulse low
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_7;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_7 then
+								if clockCycles > POWERON_CLKWAIT_7 then
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_8;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_8 then
+								if clockCycles < POWERON_CLKWAIT_8 then
+									LCDDataBus(7 downto 4) <= "0010";
+									LCD_E <= '1';			-- Set pulse high
+								else
+									LCDDataBus(7 downto 4) <= "0000";	
+									LCD_E <= '0';			-- Set pulse low
+									clockCycles := 0;
+									initLCDPowerOn <= LCD_POWERON_9;
+								end if;
+								
+							elsif initLCDPowerOn = LCD_POWERON_9 then
+								if clockCycles > POWERON_CLKWAIT_9 then
+									clockCycles := 0;
+									lcdEnableState <= LCD_ENABLED;
+									lcdWriteState <= LCD_CONFIG_FUNCTION_SET;
+								end if;
+							end if IsPoweredOn;
 							
-						elsif initLCDPowerOn = LCD_POWERON_3 then
-							if clockCycles > POWERON_CLKWAIT_3 then
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_4;
-							end if;
-							
-						elsif initLCDPowerOn = LCD_POWERON_4 then
-							if clockCycles < POWERON_CLKWAIT_4 then
-								LCDDataBus(7 downto 4) <= "0011";
-								LCD_E <= '1';			-- Set pulse high
-							else
-								LCDDataBus(7 downto 4) <= "0000";	
-								LCD_E <= '0';			-- Set pulse low
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_5;
-							end if;
-							
-						elsif initLCDPowerOn = LCD_POWERON_5 then
-							if clockCycles > POWERON_CLKWAIT_5 then
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_6;
-							end if;
-						elsif initLCDPowerOn = LCD_POWERON_6 then
-							if clockCycles < POWERON_CLKWAIT_6 then
-								LCDDataBus(7 downto 4) <= "0011";
-								LCD_E <= '1';			-- Set pulse high
-							else
-								LCDDataBus(7 downto 4) <= "0000";	
-								LCD_E <= '0';			-- Set pulse low
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_7;
-							end if;
-							
-						elsif initLCDPowerOn = LCD_POWERON_7 then
-							if clockCycles > POWERON_CLKWAIT_7 then
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_8;
-							end if;
-							
-						elsif initLCDPowerOn = LCD_POWERON_8 then
-							if clockCycles < POWERON_CLKWAIT_8 then
-								LCDDataBus(7 downto 4) <= "0010";
-								LCD_E <= '1';			-- Set pulse high
-							else
-								LCDDataBus(7 downto 4) <= "0000";	
-								LCD_E <= '0';			-- Set pulse low
-								clockCycles := 0;
-								initLCDPowerOn <= LCD_POWERON_9;
-							end if;
-							
-						elsif initLCDPowerOn = LCD_POWERON_9 then
-							if clockCycles > POWERON_CLKWAIT_9 then
-								clockCycles := 0;
-								lcdEnableState <= LCD_ENABLED;
-								lcdWriteState <= LCD_CONFIG_FUNCTION_SET;
-							end if;
-						end if PowerOnState;
+						elsif lcdEnableState = LCD_ENABLED then
 						
-					elsif lcdEnableState = LCD_ENABLED then
-					
-						WriteState : if lcdWriteState = LCD_CONFIG_FUNCTION_SET then
-							-- 0x28
-							LCDDataBus <= "00101000";
-							LCD_E <= '1';		
-							if clockCycles > CONFIG_FUNCTIONSET_CLKWAIT then
-								clockCycles := 0; 
-								lcdWriteState <= LCD_CONFIG_ENTRYMODE_SET;
-							end if;
+							WriteState : if lcdWriteState = LCD_CONFIG_FUNCTION_SET then
+								-- 0x28
+								LCDDataBus <= "00101000";
+								LCD_E <= '1';		
+								if clockCycles > CONFIG_FUNCTIONSET_CLKWAIT then
+									clockCycles := 0; 
+									lcdWriteState <= LCD_CONFIG_ENTRYMODE_SET;
+								end if;
+								
+							elsif lcdWriteState = LCD_CONFIG_ENTRYMODE_SET then
+								-- 0x06
+								LCDDataBus <= "00000110";
+								LCD_E <= '1';		
+								if clockCycles > CONFIG_ENTRYMODE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= LCD_CONFIG_DISPLAY_ONOFF;
+								end if;
+								
 							
-						elsif lcdWriteState = LCD_CONFIG_ENTRYMODE_SET then
-							-- 0x06
-							LCDDataBus <= "00000110";
-							LCD_E <= '1';		
-							if clockCycles > CONFIG_ENTRYMODE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= LCD_CONFIG_DISPLAY_ONOFF;
-							end if;
+							elsif lcdWriteState = LCD_CONFIG_DISPLAY_ONOFF then
+								-- 0x0C
+								--LCDDataBus <= "00001100";
+								LCDDataBus <= "00001100";
+								LCD_E <= '1';		
+								if clockCycles > CONFIG_DISPLAY_ONOFF_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= LCD_CONFIG_CLEAR_DISPLAY;
+								end if;	
 							
 						
-						elsif lcdWriteState = LCD_CONFIG_DISPLAY_ONOFF then
-							-- 0x0C
-							--LCDDataBus <= "00001100";
-							LCDDataBus <= "00001100";
-							LCD_E <= '1';		
-							if clockCycles > CONFIG_DISPLAY_ONOFF_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= LCD_CONFIG_CLEAR_DISPLAY;
-							end if;	
-						
-					
-						elsif lcdWriteState = LCD_CONFIG_CLEAR_DISPLAY then
-							LCDDataBus <= "00000001";
-							LCD_E <= '1';		
-							if clockCycles > CONFIG_CLEAR_DISPLAY_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= WRITE_INIT;
-							end if;
+							elsif lcdWriteState = LCD_CONFIG_CLEAR_DISPLAY then
+								LCDDataBus <= "00000001";
+								LCD_E <= '1';		
+								if clockCycles > CONFIG_CLEAR_DISPLAY_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= WRITE_INIT;
+								end if;
+								
+							elsif lcdWriteState = WRITE_INIT then
+								LCDDataBus <= "00001100";
+								LCD_RS <= '1';		
+								if clockCycles > WRITE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= W1;
+								end if;
 							
-						elsif lcdWriteState = WRITE_INIT then
-							LCDDataBus <= "00001100";
-							LCD_RS <= '1';		
-							if clockCycles > WRITE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= W1;
-							end if;
-						
-						elsif lcdWriteState = W1 then
-							LCDDataBus <= "01000110";
-							LCD_RS <= '1';		
-							if clockCycles > WRITE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= W2;
-							end if;
-						
-						elsif lcdWriteState = W2 then
-							LCDDataBus <= "01010000";
-							LCD_RS <= '1';		
-							if clockCycles > WRITE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= W3;
-							end if;
-						
-						elsif lcdWriteState = W3 then
-							LCDDataBus <= "01000111";
-							LCD_RS <= '1';			
-							if clockCycles > WRITE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= W4;
-							end if;
+							elsif lcdWriteState = W1 then
+								LCDDataBus <= "01000110";
+								LCD_RS <= '1';		
+								if clockCycles > WRITE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= W2;
+								end if;
 							
-						elsif lcdWriteState = W4 then
-							LCDDataBus <= "01000001";
-							LCD_RS <= '1';		
-							if clockCycles > WRITE_CLKWAIT then
-								clockCycles := 0;
-								lcdWriteState <= WRITE_DONE;
-							end if;
+							elsif lcdWriteState = W2 then
+								LCDDataBus <= "01010000";
+								LCD_RS <= '1';		
+								if clockCycles > WRITE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= W3;
+								end if;
 							
-						end if WriteState; 
-					end if LCDEnableDisable;
-					
-					clockCycles := clockCycles + 1;
-					
+							elsif lcdWriteState = W3 then
+								LCDDataBus <= "01000111";
+								LCD_RS <= '1';			
+								if clockCycles > WRITE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= W4;
+								end if;
+								
+							elsif lcdWriteState = W4 then
+								LCDDataBus <= "01000001";
+								LCD_RS <= '1';		
+								if clockCycles > WRITE_CLKWAIT then
+									clockCycles := 0;
+									lcdWriteState <= WRITE_DONE;
+								end if;
+							end if WriteState; 
+						end if IsReadyForWrite;
+						
+						clockCycles := clockCycles + 1;
+						
+					end if IfThereIsDataToEmit;
 				end if ResetState;
 			end if ClockSync;
 		end process;
--- InitializeCPU.vhd
+++ InitializeCPU.vhd
@@ -42,169 +42,28 @@ end InitializeCPU;
 
 architecture Behavioral of InitializeCPU is
 
+type CPU_INIT_STATE is ( CPU_INIT_STATE_DISABLED, 
+								 CPU_INIT_STATE_ENABLED
+							  );
+						
+signal cpuState : CPU_INIT_STATE;
+						
 begin
 
 	process(clock)
 	begin
-			ClockSync : if rising_edge(clock) then
+		ClockSync : if rising_edge(clock) then
 			ResetState : if reset = '1' then
+				cpuState <= CPU_INIT_STATE_DISABLED;
 			elsif reset = '0' then
-				enable_lcd <= '1';
-				firstPC <= "00000000000000000000000000000001";
+				if cpuState = CPU_INIT_STATE_DISABLED then
+					enable_lcd <= '1';
+					firstPC <= "00000000000000000000000000000001";
+					cpuState <= CPU_INIT_STATE_ENABLED;
+				end if;
 			end if ResetState; 
 		end if ClockSync;
 	end process;
---		ClockSync : if rising_edge(clock) then
---			ResetState : if reset = '1' then
---				initstate 		<= INIT_STATE_LCD;
---				initLCD 			<= LCD_START_POWERON;
---				initLCDPowerOn <= LCD_POWERON_1;
---				initLCDConfig 	<= LCD_CONFIG_FUNCTION_SET;
---				enable_lcd 		<= '1';
---				clockCycles 	:= 0;
---				opstate <= "00";
---			elsif reset = '0' then
---				InitStateStart: if initstate = INIT_STATE_LCD then
---				
---					lcd_state <= '0';
---					
---					InitStateLCD : if initLCD = LCD_START_POWERON then
---					
---						PowerOnState : if initLCDPowerOn = LCD_POWERON_1 then
---							if clockCycles > POWERON_CLKWAIT_1 then
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_2;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_2 then
---							if clockCycles < POWERON_CLKWAIT_2 then
---								LCDDataBus(7 downto 4) <= "0011";	
---								LCDControl(2) <= '1';			-- Set pulse high
---								opstate <= "01";
---							else
---								LCDDataBus(7 downto 4) <= "0000";	
---								LCDControl(2) <= '0';			-- Set pulse low
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_3;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_3 then
---							if clockCycles > POWERON_CLKWAIT_3 then
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_4;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_4 then
---							if clockCycles < POWERON_CLKWAIT_4 then
---								LCDDataBus(7 downto 4) <= "0011";
---								LCDControl(2) <= '1';			-- Set pulse high
---							else
---								LCDDataBus(7 downto 4) <= "0000";	
---								LCDControl(2) <= '0';			-- Set pulse low
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_5;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_5 then
---							if clockCycles > POWERON_CLKWAIT_5 then
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_6;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_6 then
---							if clockCycles < POWERON_CLKWAIT_6 then
---								LCDDataBus(7 downto 4) <= "0011";
---								LCDControl(2) <= '1';			-- Set pulse high
---							else
---								LCDDataBus(7 downto 4) <= "0000";	
---								LCDControl(2) <= '0';			-- Set pulse low
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_7;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_7 then
---							if clockCycles > POWERON_CLKWAIT_7 then
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_8;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_8 then
---							if clockCycles < POWERON_CLKWAIT_8 then
---								LCDDataBus(7 downto 4) <= "0010";
---								LCDControl(2) <= '1';			-- Set pulse high
---							else
---								LCDDataBus(7 downto 4) <= "0000";	
---								LCDControl(2) <= '0';			-- Set pulse low
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_9;
---							end if;
---							
---						elsif initLCDPowerOn = LCD_POWERON_9 then
---							if clockCycles > POWERON_CLKWAIT_9 then
---								clockCycles := 0;
---								initLCDPowerOn <= LCD_POWERON_1;
---								initLCD <= LCD_START_CONFIG;
---								initLCDConfig <= LCD_CONFIG_FUNCTION_SET;
---							end if;
---						end if PowerOnState;
---						
---					elsif initLCD = LCD_START_CONFIG then
---					
---						ConfigState : if initLCDConfig = LCD_CONFIG_FUNCTION_SET then
---							-- 0x28
---							LCDDataBus <= "00101000";
---							LCDControl(1) <= '0';		
---							if clockCycles > CONFIG_FUNCTIONSET_CLKWAIT then
---								clockCycles := 0; 
---								initLCDConfig <= LCD_CONFIG_ENTRYMODE_SET;
---							end if;
---							
---						elsif initLCDConfig = LCD_CONFIG_ENTRYMODE_SET then
---							-- 0x06
---							LCDDataBus <= "00000110";
---							LCDControl(1) <= '0';		
---							if clockCycles > CONFIG_ENTRYMODE_CLKWAIT then
---								clockCycles := 0;
---								initLCDConfig <= LCD_CONFIG_DISPLAY_ONOFF;
---							end if;
---							
---						
---						elsif initLCDConfig = LCD_CONFIG_DISPLAY_ONOFF then
---							-- 0x0C
---							--LCDDataBus <= "00001100";
---							LCDDataBus <= "00001000";
---							LCDControl(1) <= '0';		
---							if clockCycles > CONFIG_DISPLAY_ONOFF_CLKWAIT then
---								clockCycles := 0;
---								initLCDConfig <= LCD_CONFIG_CLEAR_DISPLAY;
---							end if;	
---						
---					
---						elsif initLCDConfig = LCD_CONFIG_CLEAR_DISPLAY then
---							LCDDataBus <= "00000001";
---							LCDControl(1) <= '0';		
---							if clockCycles > CONFIG_CLEAR_DISPLAY_CLKWAIT then
---								clockCycles := 0;
---								initstate <= INIT_STATE_CPU;
---							end if;
---						end if ConfigState; 
---					end if InitStateLCD;
---					
---					clockCycles := clockCycles + 1;
---					   
---				elsif initstate = INIT_STATE_CPU then
---					firstPC <= "00000000000000000000000000000001";
---					initstate <= INIT_STATE_DONE;
---					
---					lcd_state <= '1';
---					
---					opstate <= "10";
---					
---				end if InitStateStart;
---			end if ResetState; 
---		end if ClockSync;
---	end process;
-	
 
 end Behavioral;
 
